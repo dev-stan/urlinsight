51aa99a3e6a15cb6ccb2246ecb03a45dd91525f1
dev-stan <stachuzalewski9@gmail.com>
2026-01-04 19:17:50 -0800

    Add dependencies

---
+++ dependencies.py
@@ -0,0 +1,11 @@
+from fastapi.params import Depends
+from models import Link
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+
+def get_link_or_404(short_code: str, db: Session = Depends(get_db)) -> Link:
+    link = db.query(Link).filter(Link.short_code == short_code).first()
+    if not link:
+        raise HTTPException(status_code=404, detail="Link not found")
+    return link
--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine, Base
 from models import Link
 from schemas import LinkCreate, LinkResponse
 import string, random
+from dependencies import get_link_or_404
+from contextlib import asynccontextmanager
+from database import get_db
 
-# Create tables
-Base.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield  # needed for contextmanager
 
-app = FastAPI()
-
-# Dependency: session per request
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app = FastAPI(lifespan=lifespan)
 
 # Helper: generate random short code
 def generate_short_code(length=6):
@@ -42,8 +40,10 @@ def list_links(db: Session = Depends(get_db)):
     return links
 
 @app.get("/links/{short_code}", response_model=LinkResponse)
-def get_link(short_code: str, db: Session = Depends(get_db)):
-    link = db.query(Link).filter(Link.short_code == short_code).first()
-    if not link:
-        raise HTTPException(status_code=404, detail="Link not found")
+def get_link(short_code: str, db: Session = Depends(get_db), link: Link = Depends(get_link_or_404)):
     return link
+
+@app.get("/redirect/{short_code}", response_class=RedirectResponse, status_code=302)
+def redirect_link(short_code: str, db: Session = Depends(get_db), link: Link = Depends(get_link_or_404)):
+    return link.target_url
+
